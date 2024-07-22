a02005d504764f2537f2935962f6116ee24271f5
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-07-22 01:14:02 -0300

    minor

--- src/main.ts
+++ src/main.ts
@@ -18,16 +18,3 @@ const app = createApp(App);
 registerPlugins(app);
 
 app.mount("#app");
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      (registration) => {
-        console.log("ServiceWorker registrado com sucesso!", registration);
-      },
-      (err) => {
-        console.log("ServiceWorker falhou no registro: ", err);
-      }
-    );
-  });
-}
